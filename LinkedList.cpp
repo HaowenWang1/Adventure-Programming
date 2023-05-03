fbfee460440a62e07e536bd11ef64751c3028e83
SuzhiLi <s3789222@student.rmit.edu.au>
2023-05-03 16:15:06 +1000

    Node functions

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,5 +1,6 @@
 #include "LinkedList.h"
 
+
 LinkedList::LinkedList() {
    head = nullptr;
 
@@ -29,3 +30,30 @@ void LinkedList::addBack(Stock data)
         current->next = newNode;
     }
 }
+
+int LinkedList::size() {
+    int count = 0;
+    Node* current = head;
+    while (current != nullptr)
+    {
+        ++count;
+        current = current->next;
+    }
+    return count;
+}
+
+// Node LinkedList::get(int index){
+//     int count = 0;
+//     Node* current = head;
+//     Node* returnNode;
+//     if (index >=0 && index < size())
+//     {
+//         while(count < index)
+//         {
+//             ++count;
+//             current = current->next;
+//         }
+//         returnNode = current;
+//     }
+    
+// }
