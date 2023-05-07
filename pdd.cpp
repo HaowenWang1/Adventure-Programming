219d86c0c5eb4a476613c7d3c55337e11d042cfd
SuzhiLi <s3789222@student.rmit.edu.au>
2023-05-07 20:16:30 +1000

    new

--- pdd.cpp
+++ pdd.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <limits>
 #include "LinkedList.h"
 using namespace std;
 
@@ -284,23 +285,26 @@ int main(int argc, char **argv)
     }
     
     vector<Coin> coins = loadCoin("coins.dat");
-    cout << "Main Menu: " << endl;
-    cout << "\t1.Display Items" << endl;
-    cout << "\t2.Purchase Items" << endl;
-    cout << "\t3.Save and Exit" << endl;
-    cout << "Adminstrator-Only Menu:" << endl;
-    cout << "\t4.Add Item" << endl;
-    cout << "\t5.Remove Item" << endl;
-    cout << "\t6.Display Coins" << endl;
-    cout << "\t7.Reset Stock" << endl;
-    cout << "\t8.Reset Coins" << endl;
-    cout << "\t9. Abort Program" << endl;
+    
     
     int option = 0;
     while( option != 3)
     {   
+        cout << endl;
+        cout << "Main Menu: " << endl;
+        cout << "\t1.Display Items" << endl;
+        cout << "\t2.Purchase Items" << endl;
+        cout << "\t3.Save and Exit" << endl;
+        cout << "Adminstrator-Only Menu:" << endl;
+        cout << "\t4.Add Item" << endl;
+        cout << "\t5.Remove Item" << endl;
+        cout << "\t6.Display Coins" << endl;
+        cout << "\t7.Reset Stock" << endl;
+        cout << "\t8.Reset Coins" << endl;
+        cout << "\t9. Abort Program" << endl;
         cout << "Select your option (1-9): ";
         cin >> option;
+        cout << endl;
         if(option == 1)
         {
             stockList.PrintItems();
@@ -349,7 +353,7 @@ int main(int argc, char **argv)
         else
         {
             std::cin.clear();
-            std::cin.ignore();
+            std::cin.ignore(numeric_limits<streamsize>::max(),'\n');
             std::cout << std::endl;
             std::cout << "Please enter 1~9"<< std::endl;
             std::cout << std::endl;
