084f2613a49156ef29647d127ce57e16f84039cd
Botian Gao <s3788343@student.rmit.edu.au>
2023-05-10 17:12:12 +1000

    change main menu while loop

--- pdd.cpp
+++ pdd.cpp
@@ -285,22 +285,23 @@ int main(int argc, char **argv)
     }
     
     vector<Coin> coins = loadCoin("coins.dat");
-    cout << "Main Menu: " << endl;
-    cout << "\t1.Display Items" << endl;
-    cout << "\t2.Purchase Items" << endl;
-    cout << "\t3.Save and Exit" << endl;
-    cout << "Adminstrator-Only Menu:" << endl;
-    cout << "\t4.Add Item" << endl;
-    cout << "\t5.Remove Item" << endl;
-    cout << "\t6.Display Coins" << endl;
-    cout << "\t7.Reset Stock" << endl;
-    cout << "\t8.Reset Coins" << endl;
-    cout << "\t9. Abort Program" << endl;
     
     int option = 0;
     while( option != 3)
     {   
+        cout << "Main Menu: " << endl;
+        cout << "\t1.Display Items" << endl;
+        cout << "\t2.Purchase Items" << endl;
+        cout << "\t3.Save and Exit" << endl;
+        cout << "Adminstrator-Only Menu:" << endl;
+        cout << "\t4.Add Item" << endl;
+        cout << "\t5.Remove Item" << endl;
+        cout << "\t6.Display Coins" << endl;
+        cout << "\t7.Reset Stock" << endl;
+        cout << "\t8.Reset Coins" << endl;
+        cout << "\t9.Abort Program" << endl;
         cout << "Select your option (1-9): ";
+        
         cin >> option;
         if(option == 1)
         {
